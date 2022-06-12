0c9b39ad9f5c3112589f4ee851c3e307dbc1fe39
devdan <oluochdancun19@gmail.com>
2022-06-12 22:06:05 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+   
+}
+
+
+function setBestCustomer(){
+    return bestCustomer= 'not bob'
+}
+const bestCustomer = setBestCustomer()
+
