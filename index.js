a2500f71cb7247fdca59412f010a1f5d55e85e94
devdan <oluochdancun19@gmail.com>
2022-06-12 21:37:22 +0300

    commit tree 59954bec6ed55d39d1a3a9e00b3fd20d43cf1938

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    return window.customerName.toUpperCase()
+}
+
+upperCaseCustomerName()
+
+function setBestCustomer(){
+    return bestCustomer= 'not bob'
+}
+const bestCustomer = setBestCustomer()
+
+function overwriteBestCustomer()
